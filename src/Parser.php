6248e5f88b8d003d6db1a86c141a7867cba76a93
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2012-04-20 17:25:59 +0300

    completes the first story.

--- src/Parser.php
+++ src/Parser.php
@@ -8,21 +8,100 @@ namespace KataBankOCR;
  */
 class Parser
 {
-  public function parse($entry)
-  {
-    if (<<<PHP
- _  _  _  _  _  _  _  _  _
-| || || || || || || || || |
-|_||_||_||_||_||_||_||_||_|
+    public function parse($entry)
+    {
+        $explodedEntry = array();
+        foreach (explode(PHP_EOL, $entry) as $line) {
+            foreach (str_split($line, $length = 3) as $index => $chunk) {
+                if (false === array_key_exists($index, $explodedEntry)) {
+                    $explodedEntry[$index] = $chunk;
+                } else {
+                    $explodedEntry[$index] .= PHP_EOL . $chunk;
+                }
+            }
 
-PHP
-    == $entry
-    ) {
-      return '000000000';
+        }
+
+        $parsedEntry = '';
+        foreach ($explodedEntry as $entryChar) {
+            if (isset($this->dictionary[$entryChar])) {
+                $parsedEntry .= $this->dictionary[$entryChar];
+            }
+        }
+
+        return $parsedEntry;
     }
 
-
-
-    return '';
-  }
+    protected $dictionary = array(
+<<<PHP
+ _ 
+| |
+|_|
+   
+PHP
+        => 0,
+<<<PHP
+   
+  |
+  |
+   
+PHP
+        => 1,
+<<<PHP
+ _ 
+ _|
+|_ 
+   
+PHP
+        => 2,
+<<<PHP
+ _ 
+ _|
+ _|
+   
+PHP
+        => 3,
+<<<PHP
+   
+|_|
+  |
+   
+PHP
+        => 4,
+<<<PHP
+ _ 
+|_ 
+ _|
+   
+PHP
+        => 5,
+<<<PHP
+ _ 
+|_ 
+|_|
+   
+PHP
+        => 6,
+<<<PHP
+ _ 
+  |
+  |
+   
+PHP
+        => 7,
+<<<PHP
+ _ 
+|_|
+|_|
+   
+PHP
+        => 8,
+<<<PHP
+ _ 
+|_|
+ _|
+   
+PHP
+        => 9,
+    );
 }
